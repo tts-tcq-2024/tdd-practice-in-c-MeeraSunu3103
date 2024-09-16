a32b28c1c5941924ab4f4860e820ec17549cc766
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-09-16 17:51:31 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,14 +1,20 @@
+#include "stdlib.h"
 #include "string.h"
 
 /* Function Prototypes */
 int calculateStringSum(const char *inputString);
 int isEmptyString(const char *inputString);
+void replaceNewLineDelimiterWithCommaDelimiter(char *tempInputString);
+void addNumbersFromInputString(char *inputString, int* sumString);
 
 int calculateStringSum(const char *inputString) {
   int stringSum = 0;
+  char tempInputString[strlen(inputString)];
+  strcpy(tempInputString, inputString);
 
   if(!isEmptyString(inputString)) {
-    //tbd
+    replaceNewLineDelimiterWithCommaDelimiter(tempInputString);
+    addNumbersFromInputString(tempInputString, &stringSum);
   }
 
   return stringSum;
@@ -16,4 +22,20 @@ int calculateStringSum(const char *inputString) {
 
 int isEmptyString(const char *inputString) {
   return (strlen(inputString) == 0);
+}
+
+void addNumbersFromInputString(char *inputString, int* sumString) {
+  char* token = strtok(inputString,",");
+  while (token != NULL) {
+    (*sumString) += atoi(token);
+    token = strtok(NULL,",");
+  }
+}
+
+void replaceNewLineDelimiterWithCommaDelimiter(char *tempInputString) {
+  for(int i = 0; tempInputString[i] != '\0'; ++i) {
+    if(tempInputString[i] == '\n') {
+      tempInputString[i] = ',';
+    }
+  }
 }
