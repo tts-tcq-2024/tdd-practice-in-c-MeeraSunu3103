347581015c4747eaab379b2175054d2434e09d27
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-09-16 16:04:11 +0530

    Update StringCalculator.h
    TST01

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,1 +1,19 @@
+#include "string.h"
 
+/* Function Prototypes */
+int calculateStringSum(char *inputString);
+int isEmptyString(char *inputString);
+
+int calculateStringSum(char *inputString) {
+  int stringSum = 0;
+
+  if(!isEmptyString(inputString)) {
+    //tbd
+  }
+
+  return stringSum;
+}
+
+int isEmptyString(char *inputString) {
+  return (strlen(inputString) == 0);
+}
