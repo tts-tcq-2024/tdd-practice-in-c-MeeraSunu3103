ec2c6b9a576aa54010f97e686c2762f6fac2f88b
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-09-16 16:18:19 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -2,7 +2,7 @@
 
 /* Function Prototypes */
 int calculateStringSum(const char *inputString);
-int isEmptyString(char *inputString);
+int isEmptyString(const char *inputString);
 
 int calculateStringSum(const char *inputString) {
   int stringSum = 0;
@@ -14,6 +14,6 @@ int calculateStringSum(const char *inputString) {
   return stringSum;
 }
 
-int isEmptyString(char *inputString) {
+int isEmptyString(const char *inputString) {
   return (strlen(inputString) == 0);
 }
