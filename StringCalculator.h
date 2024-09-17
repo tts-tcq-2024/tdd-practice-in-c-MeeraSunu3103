e75dcc478183bf5e308e57cd7127ba83394e3d62
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-09-17 12:17:13 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -10,13 +10,19 @@ void addNumbersFromInputString(char *inputString, int* sumString);
 void removeNonDigitCharactersFromToken(char *token);
 void deleteCharFromStringByIndex(char *inputString, int charIndex);
 void removeNumbersGreaterThanThousand(char *token);
+void checkAndReplaceCustomDelimiterWithCommaDelimiter(char *inputString);
+void replaceCustomDelimiterWithCommaDelimiter(char *inputString, char* customDelimiter);
 
 int calculateStringSum(const char *inputString) {
   int stringSum = 0;
+  char* customDelimiter;
   char tempInputString[strlen(inputString)];
   strcpy(tempInputString, inputString);
 
   if(!isEmptyString(inputString)) {
+    printf("\n\nbefore checkAndReplaceCustomDelimiterWithCommaDelimiter: %s",tempInputString);
+    checkAndReplaceCustomDelimiterWithCommaDelimiter(tempInputString);
+    printf("\n\nafter checkAndReplaceCustomDelimiterWithCommaDelimiter: %s",tempInputString);
     replaceNewLineDelimiterWithCommaDelimiter(tempInputString);
     addNumbersFromInputString(tempInputString, &stringSum);
   }
@@ -69,10 +75,55 @@ void removeNonDigitCharactersFromToken(char *token) {
   }
 }
 
-void replaceNewLineDelimiterWithCommaDelimiter(char *tempInputString) {
-  for(int i = 0; tempInputString[i] != '\0'; ++i) {
-    if(tempInputString[i] == '\n') {
-      tempInputString[i] = ',';
+void checkAndReplaceCustomDelimiterWithCommaDelimiter(char *inputString) {
+  if((inputString[0] == '/') && (inputString[1] == '/')) {
+    char tempInputString[strlen(inputString)];
+    strcpy(tempInputString,inputString);
+    
+    char* customDelimiter = strtok(tempInputString+2,"\n");    
+    char* contentOfInputString = inputString + 2 + strlen(customDelimiter) + 1;
+    
+    replaceCustomDelimiterWithCommaDelimiter(contentOfInputString, customDelimiter);    
+    strcpy(inputString,contentOfInputString);
+  }
+}
+
+void replaceCustomDelimiterWithCommaDelimiter(char *inputString, char* customDelimiter) {
+  int lenInputString = strlen(inputString);
+  char tempString[lenInputString];
+  char tempString2[lenInputString];
+  int lenCustomDelimiter = strlen(customDelimiter);
+  
+  /* set initial values for the temporary strings */
+  strcpy(tempString2,"");
+  strcpy(tempString,inputString);
+  char* token = strstr(tempString,customDelimiter);
+    
+  while(token != NULL) {
+    /* copy the characters that occur before the custom delimiter */
+    strncpy(tempString2,tempString,(token-tempString));    
+    tempString2[(token-tempString)] = '\0';
+
+    /* add the comma delimiter */
+    strcat(tempString2,",");
+    
+    /* add the rest of the string after the custom delimiter */
+    strcat(tempString2, (token+lenCustomDelimiter));
+    
+    /* prepare for next iteration of the while loop */
+    strcpy(tempString,tempString2);    
+    token = strstr(tempString,customDelimiter);    
+    strcpy(tempString2,"");
+  }
+
+  /* finally copy the modified string into the input string */
+  strcpy(inputString,tempString);
+}
+
+void replaceNewLineDelimiterWithCommaDelimiter(char *inputString) {
+  for(int i = 0; inputString[i] != '\0'; ++i) {
+    if(inputString[i] == '\n') {
+      inputString[i] = ',';
     }
   }
 }
