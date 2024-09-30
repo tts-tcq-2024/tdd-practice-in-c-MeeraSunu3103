cd99c42f545964b6d70aae1bdb3422c5edb4a65a
MeeraSunu3103 <Sunu.Meera@in.bosch.com>
2024-09-30 10:41:59 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -10,46 +10,20 @@ TEST(StringCalculatorAddTests, ReturnsZeroForSingleZeroInput) {
     ASSERT_EQ(calculateStringSum("0"),0);
 }
 
-TEST(StringCalculatorAddTests, ReturnsSumOfInputNumbersSeparatedByCommas) {
-    ASSERT_EQ(calculateStringSum("1,2"),3);
-    ASSERT_EQ(calculateStringSum("12,1"),13);
-}
-
-TEST(StringCalculatorAddTests, ReturnsSumOfInputNumbersSeparatedByNewLines) {
-    ASSERT_EQ(calculateStringSum("1\n2"),3);
-}
-
 TEST(StringCalculatorAddTests, ReturnsSumOfInputNumbersSeparatedByCommasOrNewLines) {
     ASSERT_EQ(calculateStringSum("1,2\n3"),6);
 }
 
-TEST(StringCalculatorAddTests, ReturnsSumOfAnyNumberOfInputNumbers) {
-    char HundredOnes[201];
-    strcpy(HundredOnes,"");
-    for(int i = 0; i < 100; ++i) {
-        strcat(HundredOnes,"1,");
-    }
-    
-    ASSERT_EQ(calculateStringSum("1"),1);
-    ASSERT_EQ(calculateStringSum("1,1"),2);
-    ASSERT_EQ(calculateStringSum("1,1,1,1,1"),5);
-    ASSERT_EQ(calculateStringSum(HundredOnes),100);
-}
-
-TEST(StringCalculatorAddTests, ReturnsSumOfInputNumbersSeparatedByCustomDelimiter) {
-    ASSERT_EQ(calculateStringSum("//;\n1;2"),3);
-}
-
-TEST(StringCalculatorAddTests, ReturnsSumOfInputNumbersSeparatedByCustomDelimiterOfAnyLength) {
-    ASSERT_EQ(calculateStringSum("//;*;\n1;*;2;*;3"),6);
-}
-
 TEST(StringCalculatorAddTests, ReturnsSumOfInputNumbersSeparatedByCustomDelimiterOrNewLines) {
-    ASSERT_EQ(calculateStringSum("//;\n1;2\n3"),6);
+    ASSERT_EQ(calculateStringSum("//;;\n1;;2\n3"),6);
+}
+
+TEST(StringCalculatorAddTests, IgnoresCustomDelimiterWithoutCorrectFormat) {
+    ASSERT_EQ(calculateStringSum("//;1;1;1"),111);
 }
 
 TEST(StringCalculatorAddTests, ReturnsSumOfOnlyInputNumbersLessThanThousand) {
-    ASSERT_EQ(calculateStringSum("1002,2"),2);
+    ASSERT_EQ(calculateStringSum("1000,2"),2);
     ASSERT_EQ(calculateStringSum("1234,4567"),0);
 }
 
@@ -61,7 +35,6 @@ TEST(StringCalculatorAddTests, ReturnsZeroIfInputStringDoesNotHaveNumbers) {
 TEST(StringCalculatorAddTests, IgnoresInputCharactersThatAreNeitherNumbersNorDelimiters) {
     ASSERT_EQ(calculateStringSum("1,a,1"),2);
     ASSERT_EQ(calculateStringSum("1, 2*1"),22);
-    ASSERT_EQ(calculateStringSum("//;1;1;1"),111);
 }
 
 TEST(StringCalculatorAddTests, ThrowsExceptionForNegativeInputNumbers) {
@@ -71,6 +44,4 @@ TEST(StringCalculatorAddTests, ThrowsExceptionForNegativeInputNumbers) {
     } catch (const std::runtime_error& e) {
         ASSERT_STREQ(e.what(),"negative numbers not allowed: -1,-1");
     }
-
-    ASSERT_NO_THROW(calculateStringSum("1,1,1"));
 }
